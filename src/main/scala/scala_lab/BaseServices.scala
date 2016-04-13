d965b26aac6204936972d9c92f192c11f7bb769a
Kirill Pinchuk <cybergrind@gmail.com>
2016-04-13 17:32:06 +0300

    Define services as subclasses from akka.actor

--- src/main/scala/scala_lab/BaseServices.scala
+++ src/main/scala/scala_lab/BaseServices.scala
@@ -1,5 +1,8 @@
 package scala_lab
 
+import akka.actor._
+
+// Log base classes
 case class LogMessage (
   timestamp: Long,
   headers: Map[String, Any],
@@ -7,61 +10,84 @@ case class LogMessage (
 )
 
 class LogsBatch (messages:List[LogMessage]){
-  // HOWTO: define with only '= messages.foreach'
   def foreach(f: LogMessage => Unit):Unit = messages.foreach _
 }
 
-trait LogInput {
-  def handle(message: String){
+// Handlers/Pipeline
+trait LogInput extends Actor {
+  var destinations: List[ActorRef]
+
+  def emit(message: String):Unit = {
     val msg = LogMessage(System.currentTimeMillis, Map(), Map("body" -> message))
-    this.handle(msg)
-  }
-  def handle(headers: Map[String, Any], message:String){
-    val msg = LogMessage(System.currentTimeMillis, headers, Map("body" -> message))
-    this.handle(msg)
+    emit(msg)
   }
 
-  // Implement handler here
-  def handle(message: LogMessage)
+  def emit(headers: Map[String, Any], message:String):Unit = {
+    val msg = LogMessage(System.currentTimeMillis, headers, Map("body" -> message))
+    emit(msg)
+  }
+
+  def emit(message: LogMessage):Unit = {
+    destinations.foreach { _ ! message }
+  }
+
 }
 
-trait LogOutput {
+trait LogOutput extends Actor {
 
-  def emit(logMessage: LogMessage)
+  def receive = {
+    case msg:LogMessage => emit(msg)
+    case _ => {}
+  }
 
   def batchEmit(batch:LogsBatch){
     batch.foreach(this.emit(_))
   }
 
+  // To be defined
+  def emit(logMessage: LogMessage)
+
   def flush():Unit
 }
 
-abstract class LogHandler {
+abstract class LogHandler extends Actor {
   // To be extended with LogInput and/or LogOutput
-  val config:Map[String, Any]
-  def setNext[H](next:H)
+  var destinations: List[ActorRef]
 
-  def start():Unit
-  def stop():Unit
+  def setNext(next:ActorRef) = {
+    destinations = next :: destinations
+  }
 }
 
 abstract class Pipeline {
   // Inputs + Filters/Forwarders/ + Outputs
-  val chain:List[LogHandler]
+  val chain:List[ActorRef]
+
   def start(){
-    chain.fold(chain.head)((prev:LogHandler, curr:LogHandler) => {
+    chain.fold(chain.head)((prev:ActorRef, curr:ActorRef) => {
       if (prev != curr){
-        prev.setNext(curr)
+        prev ! ("setNext", curr)
       }
       curr
     })
   }
 }
 
+// Storage access
 abstract class LogStorage {
   def store(logMessage:LogMessage):Unit
   def store(logsBatch:LogsBatch):Unit
 
   def fetch_one():Option[LogMessage]
   def fetch_all():Array[LogMessage]
+}
+
+// HTTP traits
+trait AccessLogs {
+  def getLogs(options:Map[_, _]):LogsBatch
+
+}
+
+trait StreamLogs {
+  def streamLogs(options:Map[_, _]):Stream[LogMessage]
 }
